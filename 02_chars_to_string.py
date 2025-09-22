087a343d3cceaa18237ed9fe376c3504962e18c3
mshopovv <m.shopovv@gmail.com>
2025-09-22 12:18:37 +0300

    updating 02_char_to_string and 03_elevator

--- 02_chars_to_string.py
+++ 02_chars_to_string.py
@@ -1,3 +1,5 @@
 first_str = input()
 second_str = input()
 third_str = input()
+
+print(f"{first_character}{second_character}{third_character}")
--- 03_elevator.py
+++ 03_elevator.py
@@ -1,2 +1,8 @@
+from math import ceil
+
 all_people = int(input())
 capacity_elevator = int(input())
+
+number_of_lifts = ceil(all_people / capacity_elevator)
+
+print(number_of_lifts)
